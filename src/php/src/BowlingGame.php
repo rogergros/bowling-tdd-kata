553f6a06f7e096a81359d8f379bccfed935cd0af
Roger Gros <roger@gros.cat>
2021-12-21 23:16:12 +0100

    🟢 Step 9 / Coding spare test

--- src/php/src/BowlingGame.php
+++ src/php/src/BowlingGame.php
@@ -6,13 +6,33 @@ namespace TDD;
 
 class BowlingGame
 {
-    private int $knockedPins = 0;
+    /** @var int[] */
+    private array $rolls = [];
 
-    public function roll(int $numberOfKnockedPins) : void {
-        $this->knockedPins += $numberOfKnockedPins;
+    public function roll(int $numberOfKnockedPins): void
+    {
+        $this->rolls[] = $numberOfKnockedPins;
     }
 
-    public function score() : int {
-        return $this->knockedPins;
+    public function score(): int
+    {
+        $score = 0;
+        for ($frame = 1; $frame <= 10; ++$frame) {
+            $frameFirstBallNumber = 0 + ($frame - 1) * 2;
+            $frameSecondBallNumber = 1 + (($frame - 1) * 2);
+
+            $firstBallKnockedPins = $this->rolls[$frameFirstBallNumber] ?? 0;
+            $secondBallKnockedPins = $this->rolls[$frameSecondBallNumber] ?? 0;
+
+            $frameScore = $firstBallKnockedPins + $secondBallKnockedPins;
+
+            if ($frameScore === 10) {
+                $frameScore += $this->rolls[$frameSecondBallNumber + 1] ?? 0;
+            }
+
+            $score += $frameScore;
+        }
+
+        return $score;
     }
 }

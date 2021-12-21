693380d1c39440f82269da72566e49dbc540f435
Roger Gros <roger@gros.cat>
2021-12-21 22:23:07 +0100

    🟢 Step 7 / Refactor tests

--- src/php/src/BowlingGameTest.php
+++ src/php/src/BowlingGameTest.php
@@ -8,41 +8,44 @@ use PHPUnit\Framework\TestCase;
 
 class BowlingGameTest extends TestCase
 {
+    private BowlingGame $game;
+
+    public function setUp(): void
+    {
+        $this->game = new BowlingGame();
+    }
+
     public function testScoreWithNoThrows(): void
     {
-        $game = new BowlingGame();
-        $score = $game->score();
+        $score = $this->game->score();
 
         $this->assertEquals(0, $score);
     }
 
     public function testScoreWithOneThrowAndNoMark(): void
     {
-        $game = new BowlingGame();
-        $game->roll(5);
-        $score = $game->score();
+        $this->game->roll(5);
+        $score = $this->game->score();
 
         $this->assertEquals(5, $score);
     }
 
     public function testScoreWithTwoThrowsAndNoMark(): void
     {
-        $game = new BowlingGame();
-        $game->roll(3);
-        $game->roll(4);
-        $score = $game->score();
+        $this->game->roll(3);
+        $this->game->roll(4);
+        $score = $this->game->score();
 
         $this->assertEquals(7, $score);
     }
 
     public function testScoreWithFourThrowsAndNoMark(): void
     {
-        $game = new BowlingGame();
-        $game->roll(5);
-        $game->roll(4);
-        $game->roll(7);
-        $game->roll(2);
-        $score = $game->score();
+        $this->game->roll(5);
+        $this->game->roll(4);
+        $this->game->roll(7);
+        $this->game->roll(2);
+        $score = $this->game->score();
 
         $this->assertEquals(18, $score);
     }

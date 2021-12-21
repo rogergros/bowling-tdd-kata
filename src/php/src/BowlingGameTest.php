13de7116b450491edf4f54f9e5a80165ced92b22
Roger Gros <roger@gros.cat>
2021-12-21 22:06:19 +0100

    🔴 Step 1 / Test 1 -> Score with no throws

--- src/php/src/BowlingGameTest.php
+++ src/php/src/BowlingGameTest.php
@@ -8,5 +8,11 @@ use PHPUnit\Framework\TestCase;
 
 class BowlingGameTest extends TestCase
 {
+    public function testScoreWithNoThrows(): void
+    {
+        $game = new BowlingGame();
+        $score = $game->score();
 
+        $this->assertEquals(0, $score);
+    }
 }
